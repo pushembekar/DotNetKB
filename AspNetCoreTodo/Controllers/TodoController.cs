60ae6949b0c61a9de4f2bad923e9f7284d944d07
Pushkar Shembekar <pushembekar@gmail.com>
2018-01-13 01:13:02 -0500

    Wired up the application

--- AspNetCoreTodo/Controllers/TodoController.cs
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using AspNetCoreTodo.Services;
+using AspNetCoreTodo.Models;
 
 namespace AspNetCoreTodo.Controllers
 {
     public class TodoController : Controller
     {
-        public IActionResult Index()
+        private readonly ITodoItemService _todoItemService;
+
+        public TodoController(ITodoItemService todoItemService)
         {
-            return View();
+            _todoItemService = todoItemService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var todoItems = await _todoItemService.GetIncompleteItemAsync();
+
+            var model = new TodoViewModel()
+            {
+                Items = todoItems
+            };
+            return View(model);
         }
     }
 }
