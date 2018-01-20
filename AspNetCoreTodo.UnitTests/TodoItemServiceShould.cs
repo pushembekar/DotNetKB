551e535b3e9b12cce442ad00c8e99cf161d1cc1c
Pushkar Shembekar <pshembekar@ostglobal.com>
2018-01-19 21:41:07 -0500

    Added unit tests

---
+++ AspNetCoreTodo.UnitTests/TodoItemServiceShould.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using AspNetCoreTodo.Data;
+using AspNetCoreTodo.Models;
+using AspNetCoreTodo.Services;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace AspNetCoreTodo.UnitTests
+{
+    public class TodoItemServiceShould
+    {
+        [Fact]
+        public async Task AddNewItem()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                                    .UseInMemoryDatabase(databaseName: "Test_AddNewItem").Options;
+
+            using (var inMemoryContext = new ApplicationDbContext(options))
+            {
+                var service = new TodoItemService(inMemoryContext);
+
+                var fakeUser = new ApplicationUser
+                {
+                    Id = "fake-000",
+                    UserName = "fake@fake"
+                };
+
+                await service.AddItemAsync(new NewTodoItem { Title = "Testing?" }, fakeUser);
+            }
+
+            // Assertions
+            using (var inMemoryContext = new ApplicationDbContext(options))
+            {
+                Assert.Equal(1, await inMemoryContext.Items.CountAsync());
+
+                var item = await inMemoryContext.Items.FirstAsync();
+
+                Assert.Equal("Testing?", item.Title);
+                Assert.False(item.IsDone);
+                Assert.True(DateTimeOffset.Now.AddDays(3) - item.DueAt < TimeSpan.FromSeconds(1));
+            }
+        }
+
+        [Fact]
+        public async Task MarkDone()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                                    .UseInMemoryDatabase(databaseName: "Test_MarkDone").Options;
+
+            using (var inMemoryContext = new ApplicationDbContext(options))
+            {
+                var service = new TodoItemService(inMemoryContext);
+
+                var fakeUser = new ApplicationUser
+                {
+                    Id = "fake - 000",
+                    UserName = "fake@fake"
+                };
+
+                await service.AddItemAsync(new NewTodoItem { Title = "Testing?" }, fakeUser);
+                var item = await inMemoryContext.Items.FirstOrDefaultAsync();
+                await service.MarkDoneAsync(item.Id);
+            }
+
+            // Assertions
+            using (var inMemoryContext = new ApplicationDbContext(options))
+            {
+                Assert.Equal(1, await inMemoryContext.Items.CountAsync());
+
+                var item = await inMemoryContext.Items.FirstAsync();
+
+                Assert.Equal("Testing?", item.Title);
+                Assert.True(item.IsDone);
+                Assert.True(DateTimeOffset.Now.AddDays(3) - item.DueAt < TimeSpan.FromSeconds(1));
+            }
+        }
+    }
+}
