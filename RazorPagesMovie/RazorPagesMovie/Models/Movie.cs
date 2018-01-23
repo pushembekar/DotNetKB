e59007d87414b1d19b60cb138cc58fc87bae6cea
Pushkar Shembekar <pushembekar@gmail.com>
2018-01-23 09:43:54 -0500

    Added a DB model

---
+++ RazorPagesMovie/RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesMovie.Models
+{
+    /// <summary>
+    /// The Movie class is a Plain Old CLR class (POCO) to hold the unit of a movie
+    /// </summary>
+    public class Movie
+    {
+        // ID (Primary Key of the movie
+        public int ID { get; set; }
+        // Title of the movie
+        public string Title { get; set; }
+        // Release date of the movie
+        public DateTime ReleaseDate { get; set; }
+        // Type of the movie (genre)
+        public string Genre { get; set; }
+        // Cost of buying/renting the movie
+        public decimal Price { get; set; }
+    }
+}
---
+++ RazorPagesMovie/RazorPagesMovie/Models/MovieContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace RazorPagesMovie.Models
+{
+    /// <summary>
+    /// MovieContext class defines the database context or the "object" that will be used to talk to the database
+    /// for the entity "Movie"
+    /// </summary>
+    public class MovieContext : DbContext
+    {
+        // Dependency injection of DbContextOptions in the contructor
+        public MovieContext(DbContextOptions<MovieContext> options) : base(options)
+        {
+
+        }
+
+        // Entity to hold the list of Movies
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
