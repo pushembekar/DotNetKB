d0c7add29a7024105524065a46427f4ce1ffd24b
Pushkar Shembekar <pushembekar@gmail.com>
2018-01-24 23:34:28 -0500

    Seeding data

---
+++ RazorPagesMovie/RazorPagesMovie/Models/SeedData.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RazorPagesMovie.Models
+{
+    /// <summary>
+    /// Seeds data for the database
+    /// </summary>
+    public static class SeedData
+    {
+        /// <summary>
+        /// Initializes the database with some movies
+        /// </summary>
+        /// <param name="serviceProvider"></param>
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MovieContext(serviceProvider
+                // needs Microsoft.Extensions.DependencyInjection
+                .GetRequiredService<DbContextOptions<MovieContext>>()))
+            {
+                // Look for any movies
+                if (context.Movie.Any())
+                {
+                    return; // DB has already been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "When Harry Met Sally",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "Romantic Comedy",
+                        Price = 7.99M
+                    },
+                    new Movie
+                    {
+                        Title = "Ghostbusters",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        Price = 8.99M
+                    },
+                    new Movie
+                    {
+                        Title = "Ghostbusters 2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Comedy",
+                        Price = 9.99M
+                    },
+                    new Movie
+                    {
+                        Title = "Rio Bravo",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Western",
+                        Price = 3.99M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
