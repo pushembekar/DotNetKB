8c4432d8bafe05515d5ee7ec1169da1f6d0c93b3
Pushkar Shembekar <pushembekar@gmail.com>
2018-01-25 13:07:33 -0500

    Updating the pages

--- RazorPagesMovie/RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/RazorPagesMovie/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -15,6 +16,8 @@ namespace RazorPagesMovie.Models
         // Title of the movie
         public string Title { get; set; }
         // Release date of the movie
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         // Type of the movie (genre)
         public string Genre { get; set; }
