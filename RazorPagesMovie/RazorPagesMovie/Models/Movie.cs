b4710c8d5cdd623c601ad78ac067a90dcdf8d7eb
Pushkar Shembekar <pushembekar@gmail.com>
2018-01-27 00:27:52 -0500

    Adding validations

--- RazorPagesMovie/RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/RazorPagesMovie/Models/Movie.cs
@@ -14,16 +14,31 @@ namespace RazorPagesMovie.Models
         // ID (Primary Key of the movie
         public int ID { get; set; }
         // Title of the movie
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
+
         // Release date of the movie
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
+
         // Type of the movie (genre)
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
+
         // Cost of buying/renting the movie
+        [Range(1,100)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
+
         // Movie rating
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9-""'\s-]*$")]
+        [Required]
+        [StringLength(5)]
         public string Rating { get; set; }
     }
 }
