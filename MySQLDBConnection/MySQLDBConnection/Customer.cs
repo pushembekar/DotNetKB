de4bdac9f2e4eb67d394844907639f74d028eb60
Pushkar Shembekar <pushembekar@gmail.com>
2018-02-04 22:59:24 -0500

    MySQL database connection

---
+++ MySQLDBConnection/MySQLDBConnection/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace MySQLDBConnection
+{
+    public class Customer
+    {
+        [Key]
+        public int ID { get; set; }
+        public int Customer_id { get; set; }
+    }
+}
