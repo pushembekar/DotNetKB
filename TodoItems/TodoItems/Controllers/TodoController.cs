29bff1f42b6e9782f944247d1898e7ea387b2daf
Pushkar Shembekar <pshembekar@ostglobal.com>
2018-02-05 14:29:27 -0500

    Web Api

---
+++ TodoItems/TodoItems/Controllers/TodoController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TodoItems.Models;
+
+namespace TodoItems.Controllers
+{
+    [Route("api/[Controller]")]
+    public class TodoController : Controller
+    {
+        private readonly TodoItemContext _context;
+
+        public TodoController(TodoItemContext context)
+        {
+            _context = context;
+
+           if(_context.TodoItems.Count() == 0)
+            {
+                _context.TodoItems.Add(new TodoItem { Name = "Item1" });
+                _context.TodoItems.Add(new TodoItem { Name = "Item2" });
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return _context.TodoItems.ToList();
+        }
+
+        [HttpGet("{id}", Name = "GetTodo")]
+        public IActionResult GetById(long id)
+        {
+            var item = _context.TodoItems.FirstOrDefault(x => x.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return new ObjectResult(item);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] TodoItem item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+
+            _context.TodoItems.Add(item);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetTodo", new { id = item.Id }, item);
+        }
+    }
+}
---
+++ TodoItems/TodoItems/Models/TodoItemContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoItems.Models
+{
+    public class TodoItemContext : DbContext
+    {
+        public TodoItemContext(DbContextOptions<TodoItemContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
