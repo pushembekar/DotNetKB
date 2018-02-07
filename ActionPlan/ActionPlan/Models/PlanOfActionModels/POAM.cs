3279dc272c6f3bf298930dd03c7463ac9dfd2ed0
Pushkar Shembekar <pushembekar@gmail.com>
2018-02-06 23:42:07 -0500

    Added some POCO models

---
+++ ActionPlan/ActionPlan/Models/PlanOfActionModels/POAM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ActionPlan.Models.PlanOfActionModels
+{
+    public class POAM
+    {
+        public Guid ID { get; set; }
+        public int Number { get; set; }
+        public string CSAMPOAMID { get; set; }
+
+    }
+}
---
+++ ActionPlan/ActionPlan/Models/PlanOfActionModels/Status.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ActionPlan.Models.PlanOfActionModels
+{
+    /// <summary>
+    /// Describes the current status of the POAM
+    /// </summary>
+    public class Status
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        // ID of the status (primary key). Will need to be provided by the initializer or seeder
+        public int ID { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        // Name of the status: Planned/Pending, Existing Risk Acceptance etc.
+        public string Name { get; set; }
+
+        [MaxLength(40)]
+        // Description of the status
+        public string Description { get; set; }
+    }
+}
