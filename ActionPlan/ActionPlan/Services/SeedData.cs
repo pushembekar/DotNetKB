00c90b9015b386b1160244326b8630df5b44b1c7
Pushkar Shembekar <pshembekar@ostglobal.com>
2018-02-08 16:04:10 -0500

    Seeding data -- part 1

---
+++ ActionPlan/ActionPlan/Services/SeedData.cs
@@ -0,0 +1,52 @@
+﻿using ActionPlan.Data;
+using ActionPlan.Entities;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ActionPlan.Services
+{
+    public static class SeedData
+    {
+
+        public static void Initialize(IServiceProvider services, IHostingEnvironment env)
+        {
+            if (env.IsProduction())
+                return;
+
+            var context = services.GetRequiredService<POAMDbContext>();
+
+            if (!context.AuthSystems.Any())
+            {
+                context.AuthSystems.AddRange(
+                    new AuthSystem
+                    {
+                        Name = @"REGIS"
+                    },
+                    new AuthSystem
+                    {
+                        Name = @"REDMACS"
+                    });
+            }
+
+            if (!context.POAMs.Any())
+            {
+                var poam = new POAM
+                {
+                    AuthSystem = new AuthSystem { Name = "REGIS" },
+                    CreateDate = DateTime.Now,
+                    DelayReason = new DelayReason { Name = "Unknown" },
+                    Recommendation = @"Lorem Ipsum doler set umet",
+                    RiskLevel = new RiskLevel { Name = "VH" },
+                    Status = new Status { Name = "Planned/Pending" },
+                    Weakness = new Weakness { OriginalRecommendation = "Do it" }
+                };
+            }
+            context.SaveChanges();
+
+        }
+    }
+}
