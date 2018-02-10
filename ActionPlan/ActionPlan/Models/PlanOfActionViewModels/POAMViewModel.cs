316322181586a1efa0502f07db16b9783aa42baa
Pushkar Shembekar <pushembekar@gmail.com>
2018-02-10 10:51:45 -0500

    Added display text to column names. Hid the ID column for non-admin users

--- ActionPlan/ActionPlan/Models/PlanOfActionViewModels/POAMViewModel.cs
+++ ActionPlan/ActionPlan/Models/PlanOfActionViewModels/POAMViewModel.cs
@@ -14,66 +14,84 @@ namespace ActionPlan.Models.PlanOfActionViewModels
         public Guid ID { get; set; }
 
         [Required]
+        [Display(Name = "System")]
         // System for which the POAM has been created
         public string AuthSystem { get; set; }
 
+        [Display(Name = "Excel Line #")]
         // The number provided in the POAM excel file
         public int Number { get; set; }
 
+        [Display(Name = "CSAM POA&M ID")]
         // The POAM ID from CSAM
         public string CSAMPOAMID { get; set; }
 
+        [Display(Name = "Control ID(s)")]
         // The Control ID of the security vulnerability. 
         // This is usually the control number from the DOT compendium or CIS benchmark 
         // or a reference number from a third party entity
         public string ControlID { get; set; }
 
+        [Display(Name = "Risk Level")]
         // Risk Level as determined during the security assessment of the application/system
         public string RiskLevel { get; set; }
 
+        [Display(Name = "Status")]
         // Current status of the POAM
         public string Status { get; set; }
 
+        [Display(Name = "Delay Reason")]
         // Delay reason for the POAM (if any)
         public string DelayReason { get; set; }
 
         [Required]
+        [Display(Name = "Original Recommendation")]
         // Original recommendation provided during the security assessment F&R
         public string OriginalRecommendation { get; set; }
 
+        [Display(Name = "Risk")]
         // Detailed risk provided during the security assessment F&R
         public string Risk { get; set; }
 
         [Required]
+        [Display(Name ="Recommendation")]
         // Recommendation made during the security assessment F&R
         public string Recommendation { get; set; }
 
+        [Display(Name ="Responsible POCs")]
         // List of POCs responsible for the POAM
         public string ResponsiblePOCs { get; set; }
 
+        [Display(Name = "Resources Required")]
         [DisplayFormat(DataFormatString = "{0:C2}")]
         // Dollar amount (Resources required)
         public decimal ResourcesRequired { get; set; }
 
+        [Display(Name ="Cost Justification")]
         // Justification of the cost
         public string CostJustification { get; set; }
 
+        [Display(Name ="Scheduled Completion Date")]
         [DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:MMM yyyy}", ApplyFormatInEditMode = true, NullDisplayText = "TBD")]
         // Date the POAM is scheduled to be completed
         public DateTime? ScheduledCompletionDate { get; set; }
 
+        [Display(Name ="Planned Start")]
         [DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:MMM yyyy}", ApplyFormatInEditMode = true, NullDisplayText = "TBD")]
         // Date the POAM item is planned to start
         public DateTime? PlannedStartDate { get; set; }
 
+        [Display(Name ="Planned Finish")]
         [DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:MMM yyyy}", ApplyFormatInEditMode = true, NullDisplayText = "TBD")]
         // Date the POAM item is planned to finish
         public DateTime? PlannedFinishDate { get; set; }
 
+        [Display(Name ="Actual Start")]
         [DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:MMM yyyy}", ApplyFormatInEditMode = true, NullDisplayText = "TBD")]
         // Date the POAM work actually started
         public DateTime? ActualStartDate { get; set; }
 
+        [Display(Name ="Actual Finish")]
         [DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:MMM yyyy}", ApplyFormatInEditMode = true, NullDisplayText = "TBD")]
         // Date the POAM item work was completed
         public DateTime? ActualFinishDate { get; set; }
