4d476789d3e078083b674393eda1000b704f40e1
Pushkar Shembekar <pushkar.ctr.shembekar@faa.gov>
2018-09-05 08:42:38 -0400

    one more change

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -9,9 +9,41 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Pushkar!");
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            const int EXITCODE = 99;
+            while (true)
+            {
+                Console.WriteLine("Press " + EXITCODE + " to escape");
+                Console.WriteLine("What mathematical operation would you like to do?");
+                Console.WriteLine("1. Add");
+                Console.WriteLine("2. Subtract");
+                int option = Convert.ToInt32(Console.ReadLine());
+            
+                switch (option)
+                {
+                    case 1:
+                        Add();
+                        break;
+                    case 2:
+                        Subtract();
+                        break;
+                    case 99:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid operation\n");
+                        break;
+                }
+            }
+        }
+
+        private static void Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Subtract()
+        {
+            throw new NotImplementedException();
         }
     }
 }
